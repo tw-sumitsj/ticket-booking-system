df9d2a58219460f572f589eaa80388c33709ccb0
Sujith PS <sujith@kiebot.com>
2021-08-05 15:15:01 +0530

    Sujith Add gin to manage networking

--- app/app.go
+++ app/app.go
@@ -2,35 +2,68 @@ package app
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"net/http"
+	"time"
 )
 
 func StartServer() {
-	server := &http.Server{
-		Addr: ":8080",
-	}
+	r := gin.Default()
 
-	http.HandleFunc("/book", bookTicketHandler())
-	http.HandleFunc("/", pingHandler())
+	r.GET("/", pingHandler())
+	r.POST("/book", bookTicketHandler())
 
-	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		fmt.Errorf("something went wrong %s", err)
-		fmt.Println("Server Not Started")
-		return
-	}
-	fmt.Println("Server Started")
+	fmt.Println("Starting the Server")
+	r.Run(":8080")
 }
 
-func bookTicketHandler() func(http.ResponseWriter, *http.Request) {
-	return func(writer http.ResponseWriter, request *http.Request) {
+func pingHandler() func(context *gin.Context) {
+	return func(context *gin.Context) {
+		context.Status(http.StatusOK)
+	}
+}
+
+type Catalog struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type Ticket struct {
+	Id      int     `json:"id"`
+	Catalog Catalog `json:"catalog"`
+	Slot    Slot    `json:"slot"`
+}
+
+type Slot struct {
+	Id   int       `json:"id"`
+	Date time.Time `json:"date"`
+}
+
+type BookingResponse struct {
+	Success bool     `json:"success"`
+	Errors  []string `json:"errors"`
+	Data    []Ticket `json:"data"`
+}
+
+func bookTicketHandler() func(context *gin.Context) {
+	return func(context *gin.Context) {
 		fmt.Println("Ticket Booked")
-		writer.WriteHeader(http.StatusCreated)
-		writer.Write([]byte("Ticket Booked."))
-	}
-}
-
-func pingHandler() http.HandlerFunc {
-	return func(writer http.ResponseWriter, request *http.Request) {
-		writer.WriteHeader(http.StatusOK)
+		context.JSON(http.StatusCreated, BookingResponse{
+			Success: true,
+			Errors:  []string{},
+			Data: []Ticket{
+				{
+					Id: 0,
+					Catalog: Catalog{
+						Id:   0,
+						Name: "Movie1",
+					},
+					Slot: Slot{
+						Id:   0,
+						Date: time.Time{},
+					},
+				},
+			},
+		})
 	}
 }
